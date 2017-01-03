2e3ebbf229b9dc2adafad7391630728a8d34cd28
geethu <geethu.r@tringapps.com>
2017-01-03 18:10:50 +0530

    Initial commit

---
+++ app/src/main/java/firstprogram/com/tringapps/firstprogram/pageone_activity.java
@@ -0,0 +1,13 @@
+package firstprogram.com.tringapps.firstprogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pageone_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pageone_activity);
+    }
+}
